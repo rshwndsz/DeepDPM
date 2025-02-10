8cef12e30493ab8cbbc38f1957412b27efd8d775
Russel Dsouza <rshwndsz@gmail.com>
2025-02-10 13:14:35 +0000

    update inference script

--- scripts/DeepDPM_load_from_checkpoint.py
+++ scripts/DeepDPM_load_from_checkpoint.py
@@ -1,36 +1,44 @@
-import argparse
+from argparse import ArgumentParser, Namespace
 
 import torch
-
 from src.clustering_models.clusternet_modules.clusternetasmodel import ClusterNetModel
 from src.datasets import CustomDataset
 
-# LOAD MODEL FROM CHECKPOINT
-cp_path = "./results//model.pth" # E.g.: "./saved_models/MNIST_N2D/default_exp/epoch=57-step=31725.ckpt"
-cp_state = torch.load(cp_path)
-data_dim = 26 # E.g. for MNIST, it would be 10 if the network was trained on the embeedings supplied, or 28*28 otherwise.
-K = cp_state['state_dict']['cluster_net.class_fc2.weight'].shape[0]
-hyper_param = cp_state['hyper_parameters']
-args = argparse.Namespace()
-for key, value in hyper_param.items():
-    setattr(args, key, value)
 
-model = ClusterNetModel.load_from_checkpoint(
-    checkpoint_path=cp_path,
-    input_dim=data_dim,
-    init_k = K,
-    hparams=args
+def main(args):
+    # Load model from checkpoint
+    cp_path = args.checkpoint_path
+    cp_state = torch.load(cp_path)
+    hparams = Namespace()
+    for key, value in cp_state["hyper_parameters"].items():
+        setattr(hparams, key, value)
+    model = ClusterNetModel.load_from_checkpoint(
+        checkpoint_path=args.checkpoint_path,
+        input_dim=args.data_dim,
+        init_k=cp_state["state_dict"]["cluster_net.class_fc2.weight"].shape[0],
+        hparams=hparams,
     )
+    model.eval()
 
-# Example for inference :
-model.eval()
-dataset_obj = CustomDataset(args)
-_, val_loader = dataset_obj.get_loaders()
-cluster_assignments = []
-for data, label in val_loader:
-    soft_assign = model(data)
-    hard_assign = soft_assign.argmax(-1)
-    cluster_assignments.append(hard_assign)
-print(cluster_assignments)
+    # Inference
+    dataset_obj = CustomDataset(args)
+    _, val_loader = dataset_obj.get_loaders()
+    cluster_assignments = []
+    assert val_loader is not None
+    for data, label in val_loader:
+        soft_assign = model(data)
+        hard_assign = soft_assign.argmax(-1)
+        cluster_assignments.append(hard_assign)
+    print(cluster_assignments)
 
 
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--checkpoint_path", type=str, required=True)
+    parser.add_argument("--data-dim", type=int, default=27)
+    parser.add_argument("--data-dir", type=str, required=True)
+    parser.add_argument("--transform-input-data", type=str, default="as_is")
+    parser.add_argument("--use-labels-for-eval", action="store_true")
+
+    args = parser.parse_args()
+    main(args)
